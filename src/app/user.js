36642bcea1945c3a12d76fc71e2b522ec06690b9
honewatson <hone@hone.be>
2016-08-14 13:08:21 +1000

    add index.html

--- src/app/user.js
+++ src/app/user.js
@@ -1,9 +1,8 @@
-import pick from 'lodash.pick';
 import handler from './handler';
 
 const obj = { 'user': handler() + ' fred', 'age': 40 };
 
 console.log('hello world from user');
 
-export default pick(obj, 'user');
+export default obj; 
 
